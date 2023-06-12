10b1e50b0a48ccf18ac00e67711177fd3b855702
ajorgensen89 <ajorgensen89@gmail.com>
2023-06-12 20:57:39 +0000

    forum and booking admin

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,7 +1,33 @@
 from django.contrib import admin
-from .models import Item
-from .models import Comment
+from .models import Item, Comment
+# from .admin import bookingModel
 # Register your models here.
 
 admin.site.register(Item)
+#list_filter = ('status', 'create_on')
+
 admin.site.register(Comment)
+# @admin.register(Item)
+#class bookingModelClass(bookingModel):
+
+    # list_display = ('title', 'slug', 'status', 'create_on')
+    # search_fields = ['title', 'content']
+    #list_filter = ('status', 'create_on')
+
+
+# Register your models here.
+
+
+#@admin.register(Comment)
+#class forumCommentAdmin(admin.ModelAdmin):
+    
+    #list_display = ('name', 'body', 'post', 'create_on', 'approved')
+    #list_filter = ('approved', 'create_on')
+    #search_fields = ('name', 'email', 'body')
+
+    #action to approve comments. (more than one if needed)
+    #actions = ['approve_comments']
+
+    # boolean result. set to true.
+    #def approve_comments(self, request, queryset):
+        #queryset.update(approved=True)
--- forum/admin.py
+++ forum/admin.py
@@ -6,10 +6,28 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(forumPost)
 class forumPostAdmin(SummernoteModelAdmin):
 
+    list_display = ('title', 'slug', 'status', 'create_on')
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'create_on')
     summernote_field = ('content')
 
 
 # Register your models here.
 
 # admin.site.register(forumPost)
-admin.site.register(forumComment)
+# admin.site.register(forumComment)
+
+@admin.register(forumComment)
+class forumCommentAdmin(admin.ModelAdmin):
+    
+    list_display = ('name', 'body', 'post', 'create_on', 'approved')
+    list_filter = ('approved', 'create_on')
+    search_fields = ('name', 'email', 'body')
+
+    #action to approve comments. (more than one if needed)
+    actions = ['approve_comments']
+
+    # boolean result. set to true.
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
