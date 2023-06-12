999923029ac2d5770fa6a06f375e71253ec8d6f3
ajorgensen89 <ajorgensen89@gmail.com>
2023-06-12 18:32:04 +0000

    create new app called forum and migrate models.

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+
+STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
+
+
+class forumPost(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='forum_posts')
+    update_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    excerpt = models.TextField(blank=True)
+    create_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(User, related_name='forum_likes', blank=True)
+
+    class Meta:
+        ordering = ['-create_on']
+
+    def __str__(self):
+        return self.title
+
+    def number_of_likes(self):
+        return self.likes.count()    
+
+
+class forumComment(models.Model):
+
+    post = models.ForeignKey(forumPost, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    create_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['create_on']
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}" 
