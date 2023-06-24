80818f9231955c5185f1dfd387b738f3323b466d
ajorgensen89 <ajorgensen89@gmail.com>
2023-06-24 10:15:28 +0000

    delete unecessary field in Comments model

--- forum/models.py
+++ forum/models.py
@@ -32,7 +32,6 @@ class Comment(models.Model):
 
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     name = models.CharField(max_length=80)
-    email = models.EmailField()
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False)
